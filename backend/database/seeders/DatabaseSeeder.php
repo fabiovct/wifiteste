eee777ca9259c052ec4a8b41944bd8bbce528eae
fabiovct <vct.fabio.vct@gmail.com>
2021-07-15 21:50:14 -0300

    criado os seeds

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,81 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('produtos')->insert(array(
+            [
+            'nome' => 'Mouse',
+            'preco' => '10.00',
+            'referencia' => 'MO101'
+            ],
+        [
+            'nome' => 'Caneta',
+            'preco' => '1.50',
+            'referencia' => 'CA111',
+        ],
+        [
+            'nome' => 'Fone',
+            'preco' => '15.00',
+            'referencia' => 'FO777'
+        ],
+        [
+            'nome' => 'Tapete',
+            'preco' => '25.99',
+            'referencia' => 'TA908',
+        ],
+        [
+            'nome' => 'Cadeira',
+            'preco' => '50.00',
+            'referencia' => 'CAD54',
+        ]
+        ));
+
+        DB::table('fornecedores')->insert(array(
+            [
+            'nome' => 'FasterUp'
+            ],
+        [
+            'nome' => 'RolyX'
+        ],
+        [
+            'nome' => 'MartiMadeira'
+        ],
+        [
+            'nome' => 'Carpley'
+        ],
+        [
+            'nome' => 'Madex'
+        ]
+        ));
+
+        DB::table('produtos_fornecedores')->insert(array(
+            [
+            'produtos_id' => '1',
+            'fornecedores_id' => '1'
+            ],
+        [
+            'produtos_id' => '1',
+            'fornecedores_id' => '2'
+        ],
+        [
+            'produtos_id' => '2',
+            'fornecedores_id' => '3'
+        ],
+        [
+            'produtos_id' => '3',
+            'fornecedores_id' => '4'
+        ],
+        [
+            'produtos_id' => '4',
+            'fornecedores_id' => '5'
+        ],
+        [
+            'produtos_id' => '5',
+            'fornecedores_id' => '1'
+        ],
+        [
+            'produtos_id' => '5',
+            'fornecedores_id' => '2'
+        ]
+        ));
     }
 }
